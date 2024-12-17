90f72f3de826c0d20c830ae6c4b09cc7e6afb033
Esteban Borai <estebanborai@gmail.com>
2024-12-16 21:08:28 -0300

    fix: use `TcpStream` over `TcpListener` on TCP Waiter

--- src/resource/tcp.rs
+++ src/resource/tcp.rs
@@ -1,11 +1,9 @@
 use std::net::{IpAddr, SocketAddr};
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::time::Duration;
 
-use anyhow::{Error, Result};
-use pin_project::pin_project;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, ReadBuf};
-use tokio::net::{TcpListener, TcpStream};
+use anyhow::Result;
+use tokio::net::TcpStream;
+use tokio::time::sleep;
 
 use crate::{WaitOptions, Waitable};
 
@@ -27,101 +25,13 @@ impl TcpWaiter {
 
 impl Waitable for TcpWaiter {
     async fn wait(&self, _: &WaitOptions) -> Result<()> {
-        let tcp_listener = TcpListener::bind(self.socket()).await?;
-        let (socket, _) = tcp_listener.accept().await?;
-        let mut socket = PacketExtractor::<8>::read(socket).await?;
+        let connect = || async { TcpStream::connect(self.socket()).await };
 
-        tokio::spawn(async move {
-            let mut buf = vec![0; 1024];
-
-            loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
-
-                if n == 0 {
-                    // socket closed
-                    return;
-                }
-            }
-        })
-        .await
-        .map_err(|err| Error::msg(err.to_string()))?;
-
-        Ok(())
-    }
-}
-
-#[pin_project]
-pub struct PacketExtractor<const B: usize> {
-    pub header: [u8; B],
-    pub forwarded: usize,
-    #[pin]
-    pub socket: TcpStream,
-}
-
-impl<const B: usize> PacketExtractor<B> {
-    pub async fn read(socket: TcpStream) -> Result<Self> {
-        let mut extractor = Self {
-            header: [0; B],
-            forwarded: 0,
-            socket,
-        };
-
-        extractor.socket.read_exact(&mut extractor.header).await?;
-
-        Ok(extractor)
-    }
-
-    pub fn get_header(&mut self) -> &[u8; B] {
-        &self.header
-    }
-}
-
-impl<const B: usize> AsyncRead for PacketExtractor<B> {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buff: &mut ReadBuf<'_>,
-    ) -> Poll<std::io::Result<()>> {
-        let extractor = self.project();
-
-        if *extractor.forwarded < extractor.header.len() {
-            let leftover = &extractor.header[*extractor.forwarded..];
-            let num_forward_now = leftover.len().min(buff.remaining());
-            let forward = &leftover[..num_forward_now];
-
-            buff.put_slice(forward);
-            *extractor.forwarded += num_forward_now;
-
-            return Poll::Ready(Ok(()));
+        while let Err(err) = connect().await {
+            println!("Failed to connect to {}. {err}", self.socket());
+            sleep(Duration::from_secs(1)).await;
         }
 
-        extractor.socket.poll_read(cx, buff)
-    }
-}
-
-impl<const B: usize> AsyncWrite for PacketExtractor<B> {
-    fn poll_write(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buff: &[u8],
-    ) -> Poll<Result<usize, std::io::Error>> {
-        let extractor = self.project();
-        extractor.socket.poll_write(cx, buff)
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
-        let extractor = self.project();
-        extractor.socket.poll_flush(cx)
-    }
-
-    fn poll_shutdown(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Result<(), std::io::Error>> {
-        let extractor = self.project();
-        extractor.socket.poll_shutdown(cx)
+        Ok(())
     }
 }
